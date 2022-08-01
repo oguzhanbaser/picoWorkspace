95efe45d2cf18c9d6a6a38652c58ccb85ae82c0d
Oğuzhan Başer <baser61061@gmail.com>
2022-08-02 00:25:20 +0300

    software comment lines

--- pico-w-telegram/main.py
+++ pico-w-telegram/main.py
@@ -8,15 +8,19 @@ I2C_ADDR = 0x27
 totalRows = 2
 totalColumns = 16
 
+# telegram API key
 telegram_api_key = "YOUR API KEY"
 
+# I2C Pin Definitions
 led = Pin("LED", Pin.OUT)
 i2c = I2C(0, scl=Pin(5), sda=Pin(4), freq=100000)
 lcd = I2cLcd(i2c, I2C_ADDR, totalRows, totalColumns)
 lcd.clear()
 
+# dht11 Pin Definition
 d = dht.DHT11(machine.Pin(15))
 
+# Wifi Credentials and Wifi Conenctions
 ssid = 'WIFI_SSID'
 pswd = 'WIFI_PASWD'
 
@@ -37,15 +41,18 @@ print(wlan.ifconfig())
 lcd.clear()
 lcd.putstr("WiFi Connected")
 
+# Telegram default callback
 def get_message(message):
     lcd.clear()
     lcd.putstr(message['message']['text'])
     bot.send(message['message']['chat']['id'], message['message']['text'].upper())
 
+# send PONG text as ping reply
 def reply_ping(message):
     print(message)
     bot.send(message['message']['chat']['id'], 'pong')
     
+# send humdity value as answer
 def hum_cb(message):
     d.measure()
     lcd.clear()
@@ -54,6 +61,7 @@ def hum_cb(message):
     # print(message)
     bot.send(message['message']['chat']['id'], d.humidity())
     
+# send temp value as answer
 def temp_cb(message):
     d.measure()
     lcd.clear()
@@ -62,6 +70,7 @@ def temp_cb(message):
     # print(message)
     bot.send(message['message']['chat']['id'], d.temperature())
     
+# change led status with given parameters in message text
 def led_cb(message):
     msg = message['message']['text']
     msg_sp = msg.split(' ')
@@ -78,22 +87,23 @@ def led_cb(message):
             led.off()
             bot.send(message['message']['chat']['id'], "LED SONDU")
 
+# clear lcd screen with mesasge 
 def clear_cb(message):
     lcd.clear()            
     
-
+# start telegram bot 
 bot = utelegram.ubot(telegram_api_key)
-bot.register('/ping', reply_ping)
-bot.register('/led', led_cb)
-bot.register('/temp', temp_cb)
-bot.register('/hum', hum_cb)
-bot.register('/clear', clear_cb)
-bot.set_default_handler(get_message)
+
+bot.register('/ping', reply_ping)       # ping message callback
+bot.register('/led', led_cb)            # led message callback
+bot.register('/temp', temp_cb)          # temp message callback
+bot.register('/hum', hum_cb)            # humdity message callback
+bot.register('/clear', clear_cb)        # clear screen callbacj
+bot.set_default_handler(get_message)    # default message callback
 
 print('BOT LISTENING')
 lcd.clear()
 lcd.putstr("BOT LISTENING")
-
 
 bot.listen()
 
